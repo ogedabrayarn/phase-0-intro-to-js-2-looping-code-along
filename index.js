369f746295029d4a2696d934e1c470138f9ac8bb
Brayarn Ogeda <brayarn.oduor@student.moringaschool.com>
2022-08-18 12:59:53 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-let countDown = 10;
-while (countDown >= 0){
-    console.log(countDown--)
-}
+const namesArray = ["Guadalupe", "Ollie", "Aki"];
+function writeCards( namesArray, event ) {
+    let thankYouCards = []
+    for ( let i = 0; i < namesArray.length; i++ ) {
+      thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+    }
+    return thankYouCards;
+  }
+
+  let startingNumber = 9;
+  function countDown( startingNumber ) {
+    while ( startingNumber > 0 ) {
+      console.log( startingNumber );
+      startingNumber--;
+    }
+    console.log( startingNumber );
+  }
